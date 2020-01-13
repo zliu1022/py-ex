5336142583531c2588627afa2005a8fc67850e3b
zliu1022 <zliu1022@gmail.com>
2020-01-13 18:08:50 +0800

    Update draw_kr_batch.sh

--- draw/draw_kr_batch.sh
+++ draw/draw_kr_batch.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 currentdir=./
+echo "Drawing winrate curve in ""$currentdir"
 #lzdir=/home/zliu1022/go
 lzdir=/Users/zliu/go/leela-zero
-echo "Drawing winrate curve in ""$currentdir"
 
 for f in $currentdir/*.gz
 do
+BASENAME="${f%.*}";
+EXTENSION="${f##*.}";
+fn=$BASENAME".kr"
 #fn=$(basename "$f" | cut -d'.' -f1) #get just the filename no extension
-fn=$(basename "$f")".kr"
+echo `date +%Y-%m-%d%t%H:%M:%S`;
 echo $f, $fn
 cat gtp_0 | $lzdir/leelaz-dual -g -w "$f" > "$fn" 2>&1
 done
 
 for f in $currentdir/*
 do
-fn=$(basename "$f")".kr"
+BASENAME="${f%.*}";
+fn=$BASENAME".kr"
 python draw_kr.py "$fn"
 done

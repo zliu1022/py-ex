f84786443aa0a103342a1f91abae4e6588b79444
zliu1022 <zliu1022@gmail.com>
2020-01-22 18:20:47 +0800

    new ladder test
    standard:
    新的ladder test
    loadsgf ...sgf
    Q16 escape FAIL avoid D4 D5

--- ladder/ladder.sh
+++ ladder/ladder.sh
@@ -9,29 +9,61 @@ while read line
         then
             continue
         fi
+
         if [[ $line =~ "loadsgf" ]]
         then
-            cmd=$line"\nshowboard\nfinal_status_list ladder\n"
+            cmd="\n$line\nshowboard\nladder 1"
             arr=($line)
             sgfpath=${arr[1]}
             movenum=${arr[2]}
             sgf=${sgfpath##*/}
             path=${sgfpath%/*}
-        fi
-        if [[ $line =~ "pos" ]]
-        then
-            move=`echo ${line: 4}`
+            continue
         fi
 
-        if [[ $line =~ "escape" ]] || [[ $line =~ "capture" ]]
+        if [[ $line =~ "check" ]]
         then
-            if [[ $line =~ "escape" ]]
-            then
-                exp_ret=`echo ${line: 7}`
-            else
-                exp_ret=`echo ${line: 8}`
-            fi
-            ret=`echo -e "$cmd" | /Users/zliu/github/leela-zero/build/leelaz -g -w ~/go/weights/1.gz 2>&1 | grep "$move"`
+            move=""
+            type="" 
+            exp_ret=""
+            avoid=""
+            avoid_list=""
+
+            count=1
+            for var in ${line[@]}
+            do
+               if [[ $count == 2 ]] 
+               then
+                  move=$var
+               fi
+               if [[ $count == 3 ]] 
+               then
+                  type=$var
+               fi
+               if [[ $count == 4 ]] 
+               then
+                  exp_ret=$var
+               fi
+               if [[ $count == 5 ]] 
+               then
+                  avoid=$var
+               fi
+               if [[ $count > 5 ]] 
+               then
+                  avoid_list=$avoid_list" "$var
+               fi
+               count=`echo "$count+1"|bc`
+            done 
+            #echo 'No.' $no
+            #echo 'move      ' $move
+            #echo 'type      ' $type 
+            #echo 'result    ' $exp_ret
+            #echo 'avoid     ' $avoid
+            #echo 'avoid_list' $avoid_list
+
+            echo -e "$cmd" | /Users/zliu/github/leela-zero/build/leelaz -g -w ~/go/weights/1.gz > ladder.test.out 2>&1
+            ret=`grep "$move" ladder.test.out`
+            #echo $ret
             echo "$ret" | grep "$exp_ret" > /dev/null
             if [[ $? == 0 ]] 
             then
@@ -39,11 +71,29 @@ while read line
             else
                 result="ERR"
             fi
-            echo "No."$no $result $sgfpath $movenum
+
+            avoid=`grep "avoid" ladder.test.out`
+            #echo $avoid
+            avoid_ret=""
+            for avoid_pos in ${avoid_list[@]}
+            do
+                echo "$avoid" | grep "$avoid_pos" > /dev/null
+                if [[ $? == 0 ]] 
+                then
+                    avoid_ret=$avoid_ret" OK"
+                else
+                    avoid_ret=$avoid_ret" ERR"
+                fi
+            done 
+
+
+            echo "No."$no $result $avoid_ret $sgfpath $movenum
 
             if [[ $2 == "debug" ]]; then
                 echo "actually:" $ret
                 echo "expect  :" $exp_ret
+                echo "avoid  :"  $avoid
+                echo "expect avoid  :"  $avoid_list
                 echo
             fi
             no=`echo "$no+1"|bc`

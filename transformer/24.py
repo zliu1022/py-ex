c60a1e5a513619110423a51a4c4e4526339dea88
zliu1022 <zliu1022@gmail.com>
2023-04-19 11:39:27 +0800

    运算可以同时进行，但有错误

--- transformer/24.py
+++ transformer/24.py
@@ -18,17 +18,31 @@ def calculate(a, b, op):
             return None
         return a / b
 
+def evaluate_expression(num_permutation, op_permutation):
+    for paren_permutation in itertools.product(range(3), repeat=2):
+        expression = list(num_permutation)
+        ops = list(op_permutation)
+
+        indices = list(paren_permutation) + [2]
+
+        for index in indices:
+            result = calculate(expression[index], expression[index+1], ops[index])
+            if result is None:
+                continue
+            expression[index+1] = result
+            del expression[index]
+            del ops[index]
+            if len(expression) == 1:
+                break
+        if len(expression) == 1 and round(expression[0], 5) == 24:
+            return True
+    return False
+
 found_solution = False
 
 for num_permutation in itertools.permutations(numbers):
     for op_permutation in itertools.product(operations, repeat=3):
-        result = calculate(num_permutation[0], num_permutation[1], op_permutation[0])
-        if result is None: continue
-        result = calculate(result, num_permutation[2], op_permutation[1])
-        if result is None: continue
-        result = calculate(result, num_permutation[3], op_permutation[2])
-        if result is None: continue
-        if round(result, 5) == 24:  # rounding to avoid float imprecision
+        if evaluate_expression(num_permutation, op_permutation):
             found_solution = True
             print("Solution found:")
             print("Numbers:", num_permutation)

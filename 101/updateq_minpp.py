1a02f248ce89d233d7476c3baa044f1beb3c9cb0
zliu1022 <zliu1022@gmail.com>
2025-03-17 10:45:03 +0800

    Update updateq_minpp.py

--- 101/updateq_minpp.py
+++ 101/updateq_minpp.py
@@ -3,7 +3,20 @@
 
 import pymongo
 import json
-from matchq_v3 import canonicalize_positions
+from matchq import canonicalize_positions
+
+'''
+读取mongodb，x库，q表，里面存放的是围棋死活题
+每个document的格式如下，其中 no字段是题目编号，现在被设置称为了 unique
+
+对 q表中每一个题目，进行如下操作：
+1. 把 no字段设置成不是 unique
+2. 修改 no字段为 “url_no”
+3. 调用 match.py里的函数，计算这个题目 prepos的 “左上角初始位置” 的可哈西的3元组的元组，即stones_key = tuple(min_positions)
+4. 增加字段 min_pp，内容就是“左上角初始位置” 的可哈西的3元组的元组
+5. 设置字段 min_pp为 unique
+6. 更新到原题目中
+'''
 
 # 连接到 MongoDB
 client = pymongo.MongoClient('mongodb://localhost:27017/')

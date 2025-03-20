f7a82c427217e95a28b6273f6a44fcde6c7887a7
zliu1022 <zliu1022@gmail.com>
2025-03-20 16:34:09 +0800

    Update getq.py

--- 101/getq.py
+++ 101/getq.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 import re
 import json
 import base64
+from matchq import canonicalize_positions
+from bson import ObjectId
 
 base_dir = './.cache/'
 
@@ -40,7 +42,7 @@ def login(username):
                     for cookie in session_cookies:
                         jar.set(cookie['name'], cookie['value'], domain=cookie['domain'], path=cookie['path'])
                     session.cookies = jar
-                    #print(f"使用存储的session，用户名：{username}。")
+                    print(f"使用存储的session，用户名：{username}。")
                     return session
         else:
             print("存储的session已过期，重新登录。")
@@ -174,8 +176,31 @@ def answer_status(st):
         status = '未知'
     return status
 
-def resp_json(resp, no):
+def resp_json(resp, level_str, no):
     global base_dir
+
+    title_id = ""
+    title_level = ""
+
+    #version1:
+    #match = re.search(r'<title>Q-(\d+) -', resp.text)
+    #version2:
+    #match = re.search(r'<title>Q-(\d+) - (\d+[KDKP]) -', resp.text)
+    #    title_id = match.group(1)       # 提取 "22004"
+    #    title_level = match.group(2)    # 提取 "9K"
+
+    pattern = r"<title>\s*Q-(\d+)\s*-\s*(\S+?)\s*-"
+    match = re.search(pattern, resp.text, re.S)
+    if match:
+        title_id = match.group(1)
+        title_level = match.group(2)
+        print('html title', title_id, title_level)
+        if title_id != no:
+            print(f'warning title_id {title_level} {title_id} != url_no {no}')
+            insert_level_urlno(title_level, title_id)
+    else:
+        print('warning match title failed {level_str} {no}')
+        quit()
 
     # 提取js变量
     #re.DOTALL 标志使 . 可以匹配包括换行符在内的所有字符。
@@ -194,9 +219,24 @@ def resp_json(resp, no):
         print(f'Failed to find g_qq: {no}')
         return {'ret': False, 'code': 1}
 
-    json_name  = base_dir + no + '-' + str(obj.get('r')) + '-g_qq.json'
+    json_name  = base_dir + no + '-' + title_id + '-g_qq.json'
     with open(json_name, 'w', encoding='utf-8') as f:
         json.dump(obj, f, ensure_ascii=False, indent=4)
+
+    if obj.get('is_public') == False:
+        new_doc_id = ObjectId()
+        stones_key_list = [['_invalid_', str(new_doc_id)]]
+        doc = {
+            'url_no':     no,
+            'url_level':  level_str,
+            'title_id':   title_id,
+            'min_pp':     stones_key_list
+        }
+        print(f'Not public, insert empty p: {no}')
+        return {'ret': True, 'data': doc}
+
+    if obj.get('status') != 2:
+        print(f'status != 2 {no}')
 
     prepos = decode_prepos(obj.get('c'), obj.get('r'))
 
@@ -205,14 +245,13 @@ def resp_json(resp, no):
         swapped_prepos = [[s[1] + s[0] if len(s) == 2 else s for s in sublist] for sublist in prepos]
         prepos = swapped_prepos
 
-    opt = []
-    for opt in obj['options']:
-        e = {
-            'indexname': opt['indexname'],
-            'content':   opt['content'],
-            'isok':      opt['isok']
-        }
-        opt.append(e)
+    prepos_json = {   
+        'b': prepos[0],
+        'w': prepos[1]
+    }
+    stones_key = canonicalize_positions(prepos_json)
+    stones_key_list = [list(item) for item in stones_key]
+    stones_key_json = json.dumps(stones_key_list, sort_keys=True)
 
     ans = []
     for an in obj['answers']:
@@ -228,21 +267,24 @@ def resp_json(resp, no):
             ans.append(e)
 
     doc = {
-        'no':         no,
+        'url_no':     no,
+        'url_level':  level_str,
+        'title_id':   title_id,
+        'id':         obj.get('id'),
+        'publicid':   obj.get('publicid'),
         'size':       obj.get('lu'),
+        'status':     obj.get('status'), # 0未入库，1被淘汰，2正常
         'c':          obj.get('c'),
         'r':          obj.get('r'),
         'xv':         obj.get('xv'),
-        'prepos':     {
-            'b': prepos[0],
-            'w': prepos[1]
-            },
+        'prepos':     prepos_json, 
+        'min_pp':     stones_key_json,
         'title':      obj.get('title'),
         'blackfirst': obj.get('blackfirst'),
         'qtype':      obj.get('qtypename'),
         'level':      obj.get('levelname'),
         'name':       obj.get('name'),
-        'options':    opt,
+        'options':    obj.get('options'),
         'answers':    ans,
         'stat':       obj.get('taskresult'),
         'similar':    obj.get('sms_count'),
@@ -250,6 +292,26 @@ def resp_json(resp, no):
     }
 
     return {'ret': True, 'data': doc}
+
+# 分析title_id和url_no不一致，更新level集合
+def insert_level_urlno(level_str, url_no):
+    client = MongoClient()
+    db = client['101']
+    collection = db['level']
+
+    docs = collection.find({'level': level_str})
+    for doc in docs:
+        if 'list' in doc and url_no in doc['list']:
+            # url_no already in list, do nothing
+            print(f'{url_no} existing in level {level_str} table')
+            continue
+        else:
+            collection.update_one(
+                {'_id': doc['_id']},
+                {'$push': {'list': url_no}}
+            )
+            print(f'insert to level {level_str} table')
+    client.close()
 
 def dict_diff(old, new, path=''):
     differences = {}
@@ -268,14 +330,25 @@ def dict_diff(old, new, path=''):
             differences[current_path] = {'old': old_value, 'new': new_value}
     return differences
 
+def inc_getqnum(username):
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    db = mongo_client["101"]
+    login_collection = db["login"]
+    ret = login_collection.update_one(
+        {'username': username},
+        {'$inc': {'getq_num': 1}},
+        upsert=True # 如果文档不存在则创建
+    )
+    mongo_client.close()
+    return ret
+
 def update_q(doc):
     client = MongoClient("mongodb://localhost:27017/")
     db = client['101']
     collection = db['q']
+    #collection.create_index('no', unique=True)
 
-    collection.create_index('no', unique=True)
-
-    filter = {'no': doc['no']}
+    filter = {'min_pp': doc['min_pp']}
     old = collection.find_one_and_replace(
         filter=filter,
         replacement=doc,
@@ -291,26 +364,30 @@ def update_q(doc):
         if differences:
             print("以下字段已被更新：")
             for field, change in differences.items():
-                print(f"{field}: 从 {change['old']} 更新为 {change['new']}")
+                if field == 'stat.fail_nums' or field == 'stat.ok_nums' or field == 'r' or field == 'xv':
+                    continue
+                elif field == 'c':
+                    print(f"{field}: 从 {change['old'][0:10]} 更新为 {change['new'][0:10]}")
+                else:
+                    print(f"{field}: 从 {change['old']} 更新为 {change['new']}")
         else:
             print("数据已存在，且无任何变化。")
     else:
-        print(f"已插入新文档。{doc['no']}")
+        print(f"q集合插入新文档 {doc['url_no']}")
+    client.close()
 
-def getq(no):
+def getq(level_str, no):
     global base_dir
     base_url = "https://www.101weiqi.com/"
-    level_str = "q"
 
-    if len(sys.argv) == 2:
-        no = sys.argv[1]
     url = base_url + level_str + "/" + no
     html_name = base_dir + no + ".html"
 
-    username = 'uehbpridrciazzumpz'
+    username = 'xiuheaiswtynxvjxrx'
     session = login(username)
     if session:
         response = get_url(session, url)
+        inc_getqnum(username)
         if response:
             soup = BeautifulSoup(response.text, 'html.parser')
             pretty_html = soup.prettify()
@@ -318,23 +395,40 @@ def getq(no):
                 file.write(pretty_html)
             #print(f"页面已保存到 {html_name}。")
 
-            ret = resp_json(response, no)
+            ret = resp_json(response, level_str, no)
             if ret.get('ret'):
                 update_q(ret.get('data'))
-
-                html_name_r = base_dir + no + '-' + str(ret.get('data').get('r')) + ".html"
-                with open(html_name_r, 'w', encoding=response.encoding) as file:
-                    file.write(pretty_html)
-
                 return {'ret': True, 'data': ret.get('data')}
             else:
                 return {'ret': False, 'code': 3, 'message':'解析json失败'}
         else:
-            print("无法获取页面内容。")
+            # 如果404，估计也需要插入空记录，担心网络失败也是这样, 103905, 108916
+            new_doc_id = ObjectId()
+            stones_key_list = [['_invalid_', str(new_doc_id)]]
+            err_json = {
+                'url_no':    no,
+                'url_level':    level_str,
+                 "status":   404,
+                'min_pp':    stones_key_list
+            }
+            update_q(err_json)
+            print(f"无法获取页面内容, 插入404，{no}")
             return {'ret': False, 'code': 1, 'message':'获取页面失败'}
     else:
         print("登录失败，无法继续。")
         return {'ret': False, 'code': 2, 'message':'登录失败'}
 
 if __name__ == "__main__":
-    getq("1")
+    level_str = ''
+    url_no = ''
+    if len(sys.argv) == 3:
+        level_str = sys.argv[1]
+        url_no = sys.argv[2]
+    elif len(sys.argv) == 2:
+        url_no = sys.argv[1]
+    else:
+        print('command:')
+        print('getq level_str url_no')
+        print('getq url_no (level_str=q)')
+        quit()
+    getq(level_str, url_no)

83a5ca7625bfd5554673df8d39224f526ecc1f6d
zliu1022 <zliu1022@gmail.com>
2025-03-20 16:33:48 +0800

    Update getbook.py

--- 101/getbook.py
+++ 101/getbook.py
@@ -12,23 +12,27 @@ from bs4 import BeautifulSoup
 from getq import get_url, login, inc_getqnum
 from getonebook_cover import getonebook_cover
 from getonebook import getonebook
+from getonebook_cover_content import getonebook_cover_content
 
 from pprint import pprint
 import time
 import random
 
 '''
-从mongodb，x库，book_1集合中提取文档
-读取id到 book_url_id，
+从mongodb，x库，book_1集合中提取文档,读取id到 book_url_id，
 
-访问 https://www.101weiqi.com/book/book_url_id/，调用 get_url获取网页
+访问cover: base/book/book_url_id/，调用 get_url获取网页
 调用getonebook_cover.py，获取book_qnum，book_donenum，book_complete_url
-更新内容到 book_1 的相应文档中
+返回cover内容，有时也有q内容，更新到 book_n, 更新到 book_n_q
 
-访问 https://www.101weiqi.com/book_complete_url，调用 get_url获取网页，这也是第一页，通过第一页可以获取到下一页的页码
+访问 base/book_complete_url，调用 get_url获取网页，也是第一页
 调用 getonebook.py，获取url_level，url_no，total_pagenum
-把url_level，url_no，存入 book_1_q 集合
-访问https://www.101weiqi.com/... total_pagenum，直到完成
+把url_level，url_no，存入 book_n_q 
+继续访问... total_pagenum，直到完成
+
+异常
+- cover 404: is_share,访问complete
+- complete空: 跳过（因为已经通过cover访问过）
 '''
 
 def wait_qcounter(counter, rest_num=150):
@@ -36,12 +40,6 @@ def wait_qcounter(counter, rest_num=150):
         print(f"Reached {rest_num} calls. Waiting for 10 seconds.")
         time.sleep(10)
         counter = 0  # 重置计数器
-    '''
-    else:
-        wait_time = random.randint(1, 2)
-        print(f"Waiting for {wait_time} seconds.")
-        time.sleep(wait_time)
-    '''
     return counter+1
 
 def save_html(response, html_name):
@@ -49,7 +47,7 @@ def save_html(response, html_name):
     pretty_html = soup.prettify()
     with open(html_name, 'w', encoding=response.encoding) as file:
         file.write(pretty_html)
-    print(f"html页面保存 {html_name}。")
+    print(f"html页面保存 {html_name}")
 
 def sanitize_filename(filename):
     # Remove or replace invalid characters
@@ -63,18 +61,91 @@ def sanitize_filename(filename):
     sanitized = sanitized.replace('..', '_')
     return sanitized
 
-# 获取一个level的所有book，比如获取book_1，入门的所有棋书
-def getonebooklevel(session, documents):
-    last_id = 57503
-    last_page = 0
-    begin_status = False
+def getonebook_complete(session, book_url_id, book_complete_url):
+    print('first', book_complete_url)
+    response = get_url(session, book_complete_url)
+    if response is None:
+        print(f'fail to get complete/first {book_complete_url}')
+        quit()
+    ret = getonebook(response.text)
+    if ret:
+        op_num = batch_update(ret['url_contents'], book_url_id, book_complete_url)
+    else:
+        print(f'fail to decode {book_complete_url}')
+        quit()
+    print(f'共{ret['total_pagenum']}页')
 
-    global username
+    begin_page = 2
+    # 获取第2-total页
+    for i in range(begin_page, ret['total_pagenum']+1):
+        next_page_url = book_complete_url + '?page=' + str(i)
+        print('next', next_page_url, ret['total_pagenum'])
+        response = get_url(session, next_page_url)
+        if response is None:
+            print(f'fail to get {book_complete_url}')
+            quit()
+        ret = getonebook(response.text)
+        if ret:
+            opnum = batch_update(ret['url_contents'], book_url_id, book_complete_url)
+        else:
+            print(f'fail to decode {book_complete_url}')
+            quit()
+        getbook_counter = wait_qcounter(getbook_counter)
+
+def batch_update(ret, book_url_id, book_complete_url):
+    global book_collection
+    global curbook_collection
+
+    operations = []
+    for item in ret:
+        operations.append(
+            UpdateOne(
+                { 'url_no': item['url_no'], 'book_id': book_url_id },
+                { '$set': { 'url_level': item['url_level'], 'url_frombook': item['url_frombook'] } },
+                upsert=True
+            )
+        )
+    op_num = len(operations)
+    if operations:
+        result = book_collection.bulk_write(operations)
+        print(f"匹配 {result.matched_count}, 修改 {result.modified_count}, 新增 {result.upserted_count} {book_complete_url}")
+    else:
+        print(f'Warning: no operation, empty page {book_complete_url}')
+        #curbook_collection.update_one( {'id': book_url_id }, {'$set': { 'comment': '难度页空'}})
+    return op_num
+
+def getonebook_content(session, documents):
+    global book_collection
+    global curbook_collection
+
+    for doc in documents:
+        book_url_id = doc['id']
+        book_name = doc['name']
+        book_content = doc['cover']['content']
+        for content in book_content:
+            book_url = content['url']
+            book_complete_url = 'https://www.101weiqi.com' + book_url
+
+            response = get_url(session, book_complete_url)
+            if response is None:
+                print(f'fail to get cover {book_complete_url} is_share?')
+                continue
+            ret = getonebook_cover_content(response.text)
+            if ret:
+                op_num = batch_update(ret, book_url_id, book_complete_url)
+            else:
+                print(f'fail to decode {book_complete_url}')
+
+# 获取一个level的所有book，比如获取book_1，入门的所有棋书
+def getbook_level(session, documents):
+    last_id = 0
+    last_page = 0
+    begin_status = True
+
     global book_collection
     global curbook_collection
     getbook_counter = 0
 
-    book_cover_fail = []
     for doc in documents:
         book_url_id = doc['id']
         book_name = doc['name']
@@ -84,150 +155,52 @@ def getonebooklevel(session, documents):
                 continue
             else:
                 begin_status = True
-        '''
-        # 根据id，判断book库中是否存在同样id，有则跳过
-        ret = book_collection.find_one({'book_id': book_url_id})
-        if ret:
-            continue
-        '''
 
         # book_cover
         book_url = f'https://www.101weiqi.com/book/{book_url_id}/'
         print('cover', book_url)
 
         response = get_url(session, book_url)
-        if response is None:
-            book_cover_fail.append(book_url_id)
-            print('fail_list', book_cover_fail)
-            continue
+        if response:
+            # 先读cover再存文件,读取失败就不会覆盖上次
+            ret, ret_q = getonebook_cover(response.text)
 
-        safe_book_name = sanitize_filename(book_name)
-        html_name = '.cache/' + str(book_url_id) + '_' + safe_book_name + '.html'
-        save_html(response, html_name)
-        ret = getonebook_cover(response.text)
-        '''
-        {   'book_name':    book_name,              # 对应book_2的name
-            'book_qnum':    book_qnum,              # 对应book_2的ba.qcount
-            'book_level':   book_level,             # 对应book_2的ba.qlevelname
-            'book_donenum': book_donenum,
-            'book_complete_url': book_complete_url, # 形如/book/levelorder/361/，但21744却为空
-            'book_content':
-                {   'no':   content_no,
-                    'url':  content_url,
-                    'name': content_name,
-                    'num':  content_num } }
-        '''
-        curbook_collection.update_one(
-            {'id': book_url_id},
-            {'$set': {
-                'cover': {
-                    'qnum':        ret.get('book_qnum'),
-                    'level':       ret.get('book_level'),
-                    'donenum':     ret.get('book_donenum'),
-                    'complete_url': ret.get('book_complete_url'), #ex: '/book/levelorder/361/'
-                    'content':     ret.get('book_content')
-                    }
-            }}
-        )
-        print(f'共{ret.get('book_qnum')}题，难度{ret.get('book_level')}')
+            safe_book_name = sanitize_filename(book_name)
+            html_name = '.cache/' + str(book_url_id) + '_' + safe_book_name + '.html'
+            save_html(response, html_name)
+
+            if ret:
+                curbook_collection.update_one(
+                    {'id': book_url_id},
+                    {'$set': {
+                        'cover': {
+                            'qnum':        ret.get('book_qnum'),
+                            'level':       ret.get('book_level'),
+                            'donenum':     ret.get('book_donenum'),
+                            'complete_url': ret.get('book_complete_url'), #ex: '/book/levelorder/361/'
+                            'content':     ret.get('book_content')
+                            }
+                    }}
+                )
+                print(f'共{ret.get('book_qnum')}题，难度{ret.get('book_level')}')
+
+            if ret_q:
+                op_num = batch_update(ret_q, book_url_id, book_url)
+        else:
+            print(f'fail to get cover {book_url}')
 
         # book_complete, 第1页
-        if ret.get('book_complete_url') != '':
-            book_complete_url = 'https://www.101weiqi.com' + ret.get('book_complete_url')
-        else:
-            print(f'Info: {book_url_id} book_complete_url empty')
+        if response is None or ret is None: # 404 or is_share, try complete
+            print(f'Info: {book_url_id} book_cover empty or 404')
             book_complete_url = 'https://www.101weiqi.com/book/levelorder/' + str(book_url_id) + '/'
-        print('first', book_complete_url)
-        response = get_url(session, book_complete_url)
-        if response is None:
-            book_cover_fail.append(book_url_id)
-            print('fail_list', book_cover_fail)
-            continue
-        ret = getonebook(response.text)
-        '''
-        {   'book_title':    book_title,    # 就是name
-            'total_pagenum': total_pagenum, # 用这个
-            'cur_pagenum':   cur_pagenum,   # 没用
-            'next_pagenum':  next_pagenum,  # 没用
-            'url_contents': [
-                {
-                    'url_level':    url_level,
-                    'url_no':       url_no,
-                    'url_frombook': url    # 从book_url进入的索引序号,ex:'/book/levelorder/361/1/'
-                } }
-        '''
-        '''
-        for item in ret['url_contents']:
-            book_collection.update_one(
-                { 'url_no': item['url_no'], 'book_id': book_url_id },
-                { '$set': { 'url_level': item['url_level'], 'url_frombook': item['url_frombook'] } },
-                upsert=True
-            )
-        '''
-        operations = []
-        for item in ret['url_contents']:
-            operations.append(
-                UpdateOne(
-                    { 'url_no': item['url_no'], 'book_id': book_url_id },
-                    { '$set': { 'url_level': item['url_level'], 'url_frombook': item['url_frombook'] } },
-                    upsert=True
-                )
-            )
-        if operations:
-            result = book_collection.bulk_write(operations)
-            print(f"匹配 {result.matched_count}, 修改 {result.modified_count}, 新增 {result.upserted_count}")
         else:
-            pprint(ret)
-            print(operations)
-            print(f'Warning: no operation, empty page {book_complete_url}')
-            curbook_collection.update_one(
-                {'id': book_url_id },
-                {'$set': { 'comment': '难度页空'}}
-            )
-
-        print(f'共{ret['total_pagenum']}页')
-
-        begin_page = 2
-        if book_url_id == last_id and last_page != 0:
-            begin_page = last_page+1
-
-        # 获取第2-total页
-        for i in range(begin_page, ret['total_pagenum']+1):
-            next_page_url = book_complete_url + '?page=' + str(i)
-            print('next', next_page_url, ret['total_pagenum'])
-            response = get_url(session, next_page_url)
-            inc_getqnum(username)
-            if response is None:
-                book_cover_fail.append(book_url_id)
-                print('fail_list', book_cover_fail)
-                quit()
-            ret = getonebook(response.text)
-            '''
-            for item in ret['url_contents']:
-                book_collection.update_one(
-                    { 'url_no': item['url_no'], 'book_id': book_url_id },
-                    { '$set': { 'url_level': item['url_level'], 'url_frombook': item['url_frombook'] } },
-                    upsert=True
-                )
-            '''
-            operations = []
-            for item in ret['url_contents']:
-                operations.append(
-                    UpdateOne(
-                        { 'url_no': item['url_no'], 'book_id': book_url_id },
-                        { '$set': { 'url_level': item['url_level'], 'url_frombook': item['url_frombook'] } },
-                        upsert=True
-                    )
-                )
-            if operations:
-                result = book_collection.bulk_write(operations)
-                print(f"匹配 {result.matched_count}, 修改 {result.modified_count}, 新增 {result.upserted_count}")
+            if ret.get('book_complete_url') != '':
+                book_complete_url = 'https://www.101weiqi.com' + ret.get('book_complete_url')
             else:
-                pprint(ret)
-                print(operations)
-                quit()
+                print(f'Info: {book_url_id} book_complete_url empty, make it')
+                book_complete_url = 'https://www.101weiqi.com/book/levelorder/' + str(book_url_id) + '/'
 
-            getbook_counter = wait_qcounter(getbook_counter)
+        getonebook_complete(session, book_url_id, book_complete_url)
 
 def getbook(session):
     global book_collection
@@ -237,7 +210,7 @@ def getbook(session):
     db = client['101']
 
     books = [
-        #{'file':"入门.html", 'level':1},
+        {'file':"入门.html", 'level':1},
         {'file':"初级.html", 'level':2},
         {'file':"中级.html", 'level':3},
         {'file':"进阶.html", 'level':4},
@@ -249,11 +222,20 @@ def getbook(session):
         book_collection.create_index([('url_no', 1), ('book_id', 1)])
 
         curbook_collection = db['book_' + str(b['level'])]
-        #documents = curbook_collection.find(), 10min超时, 需要加no_cursor_timeout=True
-        documents = curbook_collection.find({}, {'_id': 0, 'id': 1, 'name': 1})
-        data_list = [{'id': doc.get('id'), 'name': doc.get('name')} for doc in documents]
 
-        getonebooklevel(session, data_list)
+        #获取这个level全部书
+        documents = curbook_collection.find({'comment': '难度页空'}, {'_id': 0, 'id': 1, 'name': 1}).sort('id', 1)
+        data_list = [{'id': doc.get('id'), 'name': doc.get('name')} for doc in documents]
+        getbook_level(session, data_list)
+
+        #获取一本book的complete页,以及后续所有页
+        #book_url_id = 3806
+        #book_complete_url = 'https://www.101weiqi.com/book/levelorder/' + str(book_url_id) + '/'
+        #getonebook_complete(session, book_url_id, book_complete_url)
+
+        #获取book的content 里面!! 内容
+        #documents = curbook_collection.find({'comment': '难度页空'}) #10min超时, 需要加no_cursor_timeout=True
+        #getonebook_content(session, documents)
         quit()
 
 if __name__ == "__main__":

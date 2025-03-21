0403a4a71153c15cc05701a0f00ca8721819976d
zliu1022 <zliu1022@gmail.com>
2025-03-21 12:44:49 +0800

    Update stat_code.py

--- file/stat_code.py
+++ file/stat_code.py
@@ -3,53 +3,68 @@
 
 import os
 
-def code_lines_count(path):
+def count_lines_in_file(file_path):
+    """统计单个文件的代码行、注释行和空行数"""
     code_lines = 0
-    comm_lines = 0
-    space_lines = 0
+    comment_lines = 0
+    empty_lines = 0
+    in_multiline_comment = False
+
+    with open(file_path, 'r', encoding='utf-8') as fp:
+        for line in fp:
+            stripped_line = line.strip()
+            if not stripped_line:  # 空行
+                empty_lines += 1
+            elif in_multiline_comment:  # 多行注释
+                comment_lines += 1
+                if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+                    in_multiline_comment = False
+            elif stripped_line.startswith('#'):  # 单行注释
+                comment_lines += 1
+            elif stripped_line.startswith("'''") or stripped_line.startswith('"""'):  # 开始多行注释
+                comment_lines += 1
+                in_multiline_comment = True
+                if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
+                    in_multiline_comment = False
+            else:  # 有效代码行
+                code_lines += 1
+
+    return code_lines, comment_lines, empty_lines
+
+def print_markdown_table(file_stats):
+    """打印Markdown格式的表格"""
+    print("| 文件名          | 代码行 | 注释行 | 空行 |")
+    print("|------------------|--------|--------|------|")
+    for file_stat in file_stats:
+        print(f"| {file_stat['name']:15s} | {file_stat['code']:6d} | {file_stat['comment']:6d} | {file_stat['empty']:4d} |")
+    print("|------------------|--------|--------|------|")
+
+    total_code = sum(stat['code'] for stat in file_stats)
+    total_comment = sum(stat['comment'] for stat in file_stats)
+    total_empty = sum(stat['empty'] for stat in file_stats)
+
+    print(f"| 总计            | {total_code:6d} | {total_comment:6d} | {total_empty:4d} |")
+
+def code_lines_count(path):
+    """统计整个目录及子目录中所有Python文件的代码行数、注释行数和空行数"""
+    file_stats = []
 
     for root, dirs, files in os.walk(path):
         for item in files:
-            file_abs_path = os.path.join(root, item)
-            postfix = os.path.splitext(file_abs_path)[1]
-            if postfix != '.py':
-                continue
+            if item.endswith('.py'):  # 只统计.py文件
+                file_abs_path = os.path.join(root, item)
+                code, comment, empty = count_lines_in_file(file_abs_path)
+                file_stats.append({
+                    'name': item,
+                    'code': code,
+                    'comment': comment,
+                    'empty': empty
+                })
 
-            # 按文件处理
-            cur_comm = 0
-            cur_code = 0
-            cur_empty = 0
-            in_multiline_comment = False
-
-            with open(file_abs_path, 'r', encoding='utf-8') as fp:
-                for line in fp:
-                    stripped_line = line.strip()
-                    if not stripped_line:  # 空行
-                        space_lines += 1
-                        cur_empty += 1
-                    elif in_multiline_comment:  # 多行注释
-                        comm_lines += 1
-                        cur_comm += 1
-                        if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
-                            in_multiline_comment = False
-                    elif stripped_line.startswith('#'):  # 单行注释
-                        comm_lines += 1
-                        cur_comm += 1
-                    elif stripped_line.startswith("'''") or stripped_line.startswith('"""'):  # 开始多行注释
-                        comm_lines += 1
-                        cur_comm += 1
-                        in_multiline_comment = True
-                        if stripped_line.endswith("'''") or stripped_line.endswith('"""'):
-                            in_multiline_comment = False
-                    else:  # 有效代码行
-                        code_lines += 1
-                        cur_code += 1
-
-            print(f'{item:15s} code {cur_code:4d} comm {cur_comm:4d} empty {cur_empty:4d}')
-
-    print(f'total           code {code_lines:4d} comm {comm_lines:4d} empty {space_lines:4d}')
-    return code_lines, comm_lines, space_lines
+    print_markdown_table(file_stats)
+    return file_stats
 
 if __name__ == '__main__':
     abs_dir = os.getcwd()
-    x, y, z = code_lines_count(abs_dir)
+    code_lines_count(abs_dir)
+

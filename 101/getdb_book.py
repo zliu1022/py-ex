6c119a888325cd485e97c9e40e1243262523594d
zliu1022 <zliu1022@gmail.com>
2025-03-23 16:50:43 +0800

    getq_url, get_bookid

---
+++ 101/getdb_book.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+from getq_v1 import login, getq_url_frombook, inc_getqnum
+import time
+import random
+import sys
+from config import db_name, base_url
+
+def wait_qcounter(counter, rest_num=150):
+    counter += 1
+    if counter == 125:
+        wait_time = 300
+        print(f"Reached 125 calls to getq. Waiting for {wait_time} seconds")
+        time.sleep(wait_time)
+        counter = 0
+    else:
+        wait_time = random.randint(15, 30)
+        print(f"Waiting for {wait_time} seconds.")
+        time.sleep(wait_time)
+    return counter
+
+def getdb_bookid(client, username, book_str, book_id):
+    session = login(client, username)
+    if session is None:
+        print("登录失败")
+        return
+
+    start_t = time.time()
+    code_1_list = []  # 获取页面失败
+    getq_counter = 0
+
+    db = client[db_name]
+    book_collection = db[book_str]
+    documents = book_collection.find({'book_id': book_id}, {'_id': 0, 'url_frombook': 1}).sort('url_frombook', 1)
+    if documents is None:
+        print(f"No document found with {book_id}")
+        return
+    data_list = [doc.get('url_frombook') for doc in documents]
+    print(f'bookid {book_id} {len(data_list)} urls')
+
+    q_collection = db['q']
+    cur_no = 0
+    for url_frombook in data_list:
+        ret = q_collection.find_one({
+            'url_frombook': url_frombook,
+            'status': {'$ne': 404}
+        })
+        if ret:
+            print(f'{url_frombook} 已经存在')
+            continue
+
+        cur_no += 1
+        print(f'{book_str} {book_id} {url_frombook} {cur_no}th')
+        result = getq_url_frombook(client, session, url_frombook)
+        inc_getqnum(client, username)
+
+        if result.get('ret') == False:
+            code = result.get('code')
+            if code == 1:
+                code_1_list.append(no)
+        getq_counter = wait_qcounter(getq_counter)
+
+    end_t = time.time()
+    print("获取页面失败（code=1）列表：", code_1_list)
+    print(f'{cur_no} done')
+    print('cost {:5.2f}s'.format(end_t - start_t))
+
+def getdb_book(username, book_str):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client[db_name]
+    book_collection = db[book_str]
+
+    documents = book_collection.distinct('book_id')
+    if documents:
+        data_list = list(documents)
+        print(f'{book_str} {len(data_list)} books')
+
+        getdb_bookid(client, username, book_str, 3764)
+        quit()
+
+        for book_id in data_list:
+            getdb_bookid(client, username, book_str, book_id)
+    else:
+        print(f"No document found with {book_str}")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        book_str = 'book_' + sys.argv[2] + '_q'
+    else:
+        quit()
+    getdb_book(username, book_str)
+    print(username, book_str, 'done')
+
--- 101/getq_v1.py
+++ 101/getq_v1.py
@@ -12,10 +12,10 @@ import json
 import base64
 from matchq import canonicalize_positions
 from bson import ObjectId
-from config import site_name, base_url, cache_dir
+from config import site_name, base_url, cache_dir, db_name
 
 def login(mongo_client, username):
-    db = mongo_client["101"]
+    db = mongo_client[db_name]
     login_collection = db["login"]
 
     # 创建用户名唯一索引（如果不存在）
@@ -187,9 +187,12 @@ def resp_json(mongo_client, resp, level_str, no):
         title_level = match.group(2)
         print('html title', title_id, title_level)
         if title_id != no:
+            #url_no和实际id不一致，url_level不一致，只要是公开，还是会跳转
+            #publicid可能是当前id，但没进行publicid和title_id一致检查，title_id只具有参考性质
             print(f'warning title_id {title_level} {title_id} != url_no {no}')
             insert_level_urlno(mongo_client, title_level, title_id)
     else:
+        #如果没有跳转到404，如果已经封杀，跳转到首页
         print('warning match title failed {level_str} {no}')
         quit()
 
@@ -210,7 +213,7 @@ def resp_json(mongo_client, resp, level_str, no):
         print(f'Failed to find g_qq: {no}')
         return {'ret': False, 'code': 1}
 
-    json_name  = cache_dir + no + '-' + title_id + '-g_qq.json'
+    json_name  = cache_dir + "/" + no + '-' + title_id + '-g_qq.json'
     with open(json_name, 'w', encoding='utf-8') as f:
         json.dump(obj, f, ensure_ascii=False, indent=4)
 
@@ -223,7 +226,7 @@ def resp_json(mongo_client, resp, level_str, no):
             'title_id':   title_id,
             'min_pp':     stones_key_list
         }
-        print(f'Not public, insert empty p: {no}')
+        print(f'Not public, insert empty p: {no} {title_id}')
         return {'ret': True, 'data': doc}
 
     if obj.get('status') != 2:
@@ -286,7 +289,7 @@ def resp_json(mongo_client, resp, level_str, no):
 
 # 分析title_id和url_no不一致，更新level集合
 def insert_level_urlno(mongo_client, level_str, url_no):
-    db = mongo_client['101']
+    db = mongo_client[db_name]
     collection = db['level']
 
     docs = collection.find({'level': level_str})
@@ -320,7 +323,7 @@ def dict_diff(old, new, path=''):
     return differences
 
 def inc_getqnum(mongo_client, username):
-    db = mongo_client["101"]
+    db = mongo_client[db_name]
     login_collection = db["login"]
     ret = login_collection.update_one(
         {'username': username},
@@ -330,7 +333,7 @@ def inc_getqnum(mongo_client, username):
     return ret
 
 def update_q(mongo_client, doc):
-    db = mongo_client['101']
+    db = mongo_client[db_name]
     collection = db['q']
     #collection.create_index('no', unique=True)
 
@@ -359,45 +362,171 @@ def update_q(mongo_client, doc):
         else:
             print("数据已存在，且无任何变化。")
     else:
-        print(f"q集合插入新文档 {doc['url_no']}")
+        print(f"q集合插入新文档 {doc['publicid']}")
+
+def resp_json_url_frombook(mongo_client, resp, url_frombook):
+    title_id = ""
+    title_level = ""
+
+    pattern = r"<title>\s*Q-(\d+)\s*-\s*(\S+?)\s*-"
+    match = re.search(pattern, resp.text, re.S)
+    if match:
+        title_id = match.group(1)
+        title_level = match.group(2)
+        print('从html提取 id和level', title_id, title_level)
+    else:
+        #如果没有跳转到404，如果已经封杀，跳转到首页
+        print('warning match title failed {url_frombook}')
+        quit()
+
+    # 提取js变量
+    #re.DOTALL 标志使 . 可以匹配包括换行符在内的所有字符。
+    #.*? 是非贪婪匹配，尽可能少地匹配字符，直到找到第一个 };。
+    pattern = r'var\s+g_qq\s*=\s*\{.*?\};'
+    match = re.search(pattern, resp.text, re.DOTALL)
+    if match:
+        g_qq_str = match.group()
+        json_str = g_qq_str[11:-1] # 去除开头的 var g_qq =，以及最后的分号
+        try:
+            obj = json.loads(json_str)
+        except json.JSONDecodeError:
+            print(f'Failed to decode json: {json_str}')
+            return {'ret': False, 'code': 2}
+    else:
+        print(f'Failed to find g_qq: {url_frombook}')
+        return {'ret': False, 'code': 1}
+
+    if obj.get('is_public') == False:
+        new_doc_id = ObjectId()
+        stones_key_list = [['_invalid_', str(new_doc_id)]]
+        doc = {
+            'url_frombook':  url_frombook,
+            'title_id':   title_id,
+            'min_pp':     stones_key_list
+        }
+        print(f'Not public, insert empty p: {url_frombook} {title_id}')
+        return {'ret': True, 'data': doc}
+
+    if obj.get('status') != 2:
+        # status：0审核，1淘汰，2入库, 大量!=2可能异常
+        print(f'status != 2 {url_frombook}')
+
+    prepos = decode_prepos(obj.get('c'), obj.get('r'))
+
+    # 对每个字符串进行字符位置交换
+    if obj.get('xv') and obj.get('xv') % 3 != 0:
+        swapped_prepos = [[s[1] + s[0] if len(s) == 2 else s for s in sublist] for sublist in prepos]
+        prepos = swapped_prepos
+
+    prepos_json = {   
+        'b': prepos[0],
+        'w': prepos[1]
+    }
+    stones_key = canonicalize_positions(prepos_json)
+    stones_key_list = [list(item) for item in stones_key]
+    stones_key_json = json.dumps(stones_key_list, sort_keys=True)
+
+    ans = []
+    for an in obj['answers']:
+        if an['ty'] == 1 or an['ty'] == 2 and an['st'] == 2:
+            anseq = []
+            for p in an['pts']:
+                anseq.append(p['p'])
+            e = {
+                'ty': an['ty'],
+                'st': an['st'],
+                'p':  anseq
+            }
+            ans.append(e)
+
+    doc = {
+        'url_frombook':  url_frombook,
+        'title_id':   title_id,
+        'id':         obj.get('id'),
+        'publicid':   obj.get('publicid'), #唯一id，但不一定能通过level/id访问
+        'size':       obj.get('lu'),
+        'status':     obj.get('status'), # 0未入库，1被淘汰，2正常
+        'c':          obj.get('c'),
+        'r':          obj.get('r'),
+        'xv':         obj.get('xv'),
+        'prepos':     prepos_json, 
+        'min_pp':     stones_key_json,
+        'title':      obj.get('title'),
+        'blackfirst': obj.get('blackfirst'),
+        'qtype':      obj.get('qtypename'),
+        'level':      obj.get('levelname'), #level
+        'name':       obj.get('name'),
+        'options':    obj.get('options'),
+        'answers':    ans,
+        'stat':       obj.get('taskresult'),
+        'similar':    obj.get('sms_count'),
+        'bookinfos':    obj.get('bookinfos')
+    }
+
+    return {'ret': True, 'data': doc}
+
+#q库唯一索引只有minpp
+#从url_level, url_no抓取，就更新url_level, url_no
+#从url_frombook抓取，就更新url_frombook
+def getq_url_frombook(mongo_client, session, url_frombook):
+    url = base_url + url_frombook
+    response = get_url(session, url)
+    if response:
+        ret = resp_json_url_frombook(mongo_client, response, url_frombook)
+        if ret.get('ret'):
+            update_q(mongo_client, ret.get('data'))
+            return {'ret': True, 'data': ret.get('data')}
+        else:
+            return {'ret': False, 'code': 3, 'message':'解析json失败'}
+    else:
+        # 404，插入空记录，网络失败可能也是这个
+        new_doc_id = ObjectId()
+        stones_key_list = [['_invalid_', str(new_doc_id)]]
+        err_json = {
+            'url_frombook': url_frombook,
+             "status":      404,
+            'min_pp':       stones_key_list
+        }
+        update_q(mongo_client, err_json)
+        print(f"无法获取页面内容, 插入404，{url_frombook}")
+        return {'ret': False, 'code': 1, 'message':'获取页面失败'}
 
 def getq(mongo_client, username, level_str, no):
     url = base_url + "/" + level_str + "/" + no
-    html_name = cache_dir + no + ".html"
+    html_name = cache_dir + "/" + no + ".html"
 
     session = login(mongo_client, username)
-    if session:
-        response = get_url(session, url)
-        inc_getqnum(mongo_client, username)
-        if response:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            pretty_html = soup.prettify()
-            with open(html_name, 'w', encoding=response.encoding) as file:
-                file.write(pretty_html)
-            #print(f"页面已保存到 {html_name}。")
-
-            ret = resp_json(mongo_client, response, level_str, no)
-            if ret.get('ret'):
-                update_q(mongo_client, ret.get('data'))
-                return {'ret': True, 'data': ret.get('data')}
-            else:
-                return {'ret': False, 'code': 3, 'message':'解析json失败'}
-        else:
-            # 如果404，估计也需要插入空记录，担心网络失败也是这样, 103905, 108916
-            new_doc_id = ObjectId()
-            stones_key_list = [['_invalid_', str(new_doc_id)]]
-            err_json = {
-                'url_no':    no,
-                'url_level':    level_str,
-                 "status":   404,
-                'min_pp':    stones_key_list
-            }
-            update_q(mongo_client, err_json)
-            print(f"无法获取页面内容, 插入404，{no}")
-            return {'ret': False, 'code': 1, 'message':'获取页面失败'}
-    else:
+    if session is None:
         print("登录失败，无法继续。")
         return {'ret': False, 'code': 2, 'message':'登录失败'}
+
+    response = get_url(session, url)
+    inc_getqnum(mongo_client, username)
+    if response:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pretty_html = soup.prettify()
+        with open(html_name, 'w', encoding=response.encoding) as file:
+            file.write(pretty_html)
+
+        ret = resp_json(mongo_client, response, level_str, no)
+        if ret.get('ret'):
+            update_q(mongo_client, ret.get('data'))
+            return {'ret': True, 'data': ret.get('data')}
+        else:
+            return {'ret': False, 'code': 3, 'message':'解析json失败'}
+    else:
+        # 404，插入空记录，网络失败可能也是这个
+        new_doc_id = ObjectId()
+        stones_key_list = [['_invalid_', str(new_doc_id)]]
+        err_json = {
+            'url_no':    no,
+            'url_level':    level_str,
+             "status":   404,
+            'min_pp':    stones_key_list
+        }
+        update_q(mongo_client, err_json)
+        print(f"无法获取页面内容, 插入404，{no}")
+        return {'ret': False, 'code': 1, 'message':'获取页面失败'}
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
